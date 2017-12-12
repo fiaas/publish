d6d4b85ea80fb69d4d90cba7d0853d062e13111a
fimojoha <morten.lied.johansen@finn.no>
2017-12-12 15:47:55 +0100

    Initial PR, pulled from k8s library

---
+++ setup.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# -*- coding: utf-8
+
+import os
+from setuptools import setup
+
+GENERIC_REQ = [
+    "six==1.10.0",
+    'GitPython==2.1.8',
+    "twine==1.9.1"
+]
+
+CODE_QUALITY_REQ = [
+    'prospector',
+]
+
+TESTS_REQ = [
+    'tox==2.7.0',
+    'mock==2.0.0',
+    'pytest==3.3.1',
+    'pytest-cov==2.5.1',
+    'pytest-html==1.16.0',
+    'pytest-sugar==0.9.0',
+]
+
+
+def _generate_description():
+    description = [_read("README.rst")]
+    changelog_file = os.getenv("CHANGELOG_FILE")
+    if changelog_file:
+        description.append(_read(changelog_file))
+    return "\n".join(description)
+
+
+def _get_license_name():
+    with open(os.path.join(os.path.dirname(__file__), "LICENSE")) as f:
+        for line in f:
+            if line.strip():
+                return line.strip()
+
+
+def _read(filename):
+    with open(os.path.join(os.path.dirname(__file__), filename)) as f:
+        return f.read()
+
+
+def main():
+    setup(
+        name="publish",
+        author="FiaaS developers",
+        author_email="fiaas@googlegroups.com",
+        use_scm_version=True,
+        py_modules=["publish", "get_github_release"],
+        zip_safe=True,
+        include_package_data=True,
+
+        # Requirements
+        install_requires=GENERIC_REQ,
+        setup_requires=['pytest-runner', 'wheel', 'setuptools_scm'],
+        extras_require={
+            "dev": TESTS_REQ + CODE_QUALITY_REQ,
+            "codacy": ["codacy-coverage"],
+            "docs": ["Sphinx>=1.6.3"]
+        },
+
+        # Metadata
+        description="Publish python package to PyPI and Github",
+        long_description=_generate_description(),
+        url="https://github.com/fiaas/publish",
+        license=_get_license_name(),
+        keywords="pypi github fiaas",
+        classifiers=[
+            "Development Status :: 3 - Alpha",
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Topic :: Internet",
+            "Topic :: Software Development :: Build Tools",
+            "Topic :: System :: Software Distribution",
+        ]
+    )
+
+
+if __name__ == "__main__":
+    main()
